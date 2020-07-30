a0aa86dbd2598f776cee0a7ea8f585b66a0d4561
ilhambara <g2a.ilhammubarok@gmail.com>
2020-07-30 15:04:57 +0700

    Fix load dan file js

--- script.js
+++ script.js
@@ -23,44 +23,13 @@ var canvas = document.getElementById('canvas'),
     ctx = canvas.getContext('2d'),
     // full screen dimensions
     cw = window.innerWidth,
-    ch = window.innerHeight,
-    // firework collection
-    fireworks = [],
-    // particle collection
-    particles = [],
-    // starting hue
-    hue = 120,
-    // when launching fireworks with a click, too many get launched at once without a limiter, one launch per 5 loop ticks
-    limiterTotal = 5,
-    limiterTick = 0,
-    // this will time the auto launches of fireworks, one launch per 80 loop ticks
-    timerTotal = 80,
-    timerTick = 0,
-    mousedown = false,
-    // mouse x coordinate,
-    mx,
-    // mouse y coordinate
-    my;
+    ch = window.innerHeight;
 
 // set canvas dimensions
 canvas.width = cw;
 canvas.height = ch;
 
 // now we are going to setup our function placeholders for the entire demo
-
-// get a random number within a range
-function random(min, max) {
-    return Math.random() * (max - min) + min;
-}
-
-// calculate the distance between two points
-function calculateDistance(p1x, p1y, p2x, p2y) {
-    var xDistance = p1x - p2x,
-        yDistance = p1y - p2y;
-    return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
-}
-
-
 
 window.onload = function () {
     var merrywrap = document.getElementById("merrywrap");
@@ -88,9 +57,7 @@ window.onload = function () {
         setTimeout(openBox, stepMinutes[step - 1]);
         step++;
     }
-
     init();
-
 }
 
 function reveal() {
@@ -105,11 +72,4 @@ function reveal() {
     else {
         w = 255; h = 155;
     }
-
-    var ifrm = document.createElement("iframe");
-    ifrm.setAttribute("src", "https://www.youtube.com/embed/KDxJlW6cxRk?controls=0&loop=1&autoplay=1");
-    //ifrm.style.width = `${w}px`;
-    //ifrm.style.height = `${h}px`;
-    ifrm.style.border = 'none';
-    document.querySelector('#video').appendChild(ifrm);
 }
